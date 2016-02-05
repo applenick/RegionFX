643d2f1b8283918ef147ac8c1f52d974032b1f34
applenick <applenick3@me.com>
2016-02-04 17:33:08 -0800

    Remove from Regions if new

--- src/main/java/com/applenick/RegionFX/tasks/EffectTask.java
+++ src/main/java/com/applenick/RegionFX/tasks/EffectTask.java
@@ -17,7 +17,7 @@ public class EffectTask implements Runnable {
 	private int taskID;
 
 	public EffectTask(){
-		RegionFX.get().getServer().getScheduler().scheduleSyncRepeatingTask(RegionFX.get(), this, 0L, 20L);
+		RegionFX.get().getServer().getScheduler().scheduleSyncRepeatingTask(RegionFX.get(), this, 0L, 10L);
 	}
 
 	public void cancle(){
@@ -29,10 +29,22 @@ public class EffectTask implements Runnable {
 		for(Player player : RegionFX.get().getServer().getOnlinePlayers()){
 			for(ProtectedRegion region : RegionFX.get().getEffectRegionManager().getLoadedRegions().keySet()){
 				if(RegionFX.get().getEffectRegionManager().insideRegion(player, region)){
+					
+					
 					if(!RegionFX.get().getEffectRegionManager().isPlayerEffected(player)){
 						EffectRegion er = RegionFX.get().getEffectRegionManager().getLoadedRegions().get(region);
 						RegionFX.get().getEffectRegionManager().addEffectedPlayer(new EffectPlayer(player , er));
 						continue;
+					}else{
+						EffectPlayer ep = RegionFX.get().getEffectRegionManager().getEffectPlayer(player);
+						if(ep.getRegion().getRegion() != region){
+							//Remove player from an overlapping Region & Add new Region
+							ep.removeEffects();
+							RegionFX.get().getEffectRegionManager().removeEffectedPlayer(ep);
+
+							EffectRegion er = RegionFX.get().getEffectRegionManager().getLoadedRegions().get(region);
+							RegionFX.get().getEffectRegionManager().addEffectedPlayer(new EffectPlayer(player , er));
+						}
 					}
 				}else{
 					if(RegionFX.get().getEffectRegionManager().isPlayerEffected(player)){
